26349f34956207373a67798b3ab574f9f44d9d2a
Nguyễn Đình Huy <151980955+huynguyendinhhcmut@users.noreply.github.com>
2024-05-07 19:48:24 +0700

    Add files via upload

--- wrapper_lab5.sv
+++ wrapper_lab5.sv
@@ -1,9 +1,10 @@
 module wrapper_lab5 (
 	input logic CLOCK_50,
+	input logic [9:9] SW,
 	input logic [0:0] KEY,
 	output logic [8:0] LEDR
 );
 
-system system1 (.clk(CLOCK_50), .resetn(KEY[0]), .LEDS(LEDR[8:0]));
+system system1 (.clk(CLOCK_50), .resetn(KEY[0]), .run(SW[9]), .LEDS(LEDR[8:0]));
 
 endmodule
